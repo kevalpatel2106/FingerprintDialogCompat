cb7cafa93f48161b0eeb7854b9df2e130b2418af
Keval Patel <kevalpatel2106@gmail.com>
2018-04-09 18:01:52 +0530

    Adding bintray integration

--- fingerprint-dialog-compat/src/test/java/com/kevalpatel2106/fingerprintdialog/ExampleUnitTest.java
+++ fingerprint-dialog-compat/src/test/java/com/kevalpatel2106/fingerprintdialog/ExampleUnitTest.java
@@ -15,7 +15,7 @@ package com.kevalpatel2106.fingerprintdialog;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
